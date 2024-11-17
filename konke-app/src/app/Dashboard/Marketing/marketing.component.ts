36fdf6e56180f99ac8ee76c0a35d5c26f7015b74
Lunga Tsewu <1998tsewu@gmail.com>
2024-11-17 05:31:31 +0200

    RoutesSetup

---
+++ konke-app/src/app/Dashboard/Marketing/marketing.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'marketing-component',
+  templateUrl:'./marketing.component.html'
+})
+
+export class MarketingComponent {}
---
+++ konke-app/src/app/Products/products.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'products-component',
+  templateUrl:'./products.component.html'
+})
+
+export class ProductComponent {}
