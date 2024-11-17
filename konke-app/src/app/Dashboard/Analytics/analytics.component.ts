b35843ae75f410594f56aedb0d6a490d086c415f
SbaRou339 <sbaviaroux@gmail.com>
2024-11-17 08:50:22 +0200

    Added analytics component

--- konke-app/src/app/Dashboard/Analytics/analytics.component.ts
+++ konke-app/src/app/Dashboard/Analytics/analytics.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'analytics-component',
-  templateUrl:'./analytics.component.html'
+  templateUrl:'./analytics.component.html',
+  styleUrls: ['analytics.component.css']
 })
 
 export class AnalyticsCOmponent {}
